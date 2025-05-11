6d77344b73053e53f64b69ad1f0f54e6751661dd
Michelangelo Media <losjuegosdeaxl@gmail.com>
2025-05-11 16:36:16 -0300

    fix backend deploy v2

--- backend/routes/movimientos.py
+++ backend/routes/movimientos.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
-from models import Movimiento
-from db import coleccion
+from backend.models import Movimiento
+from backend.db import coleccion
 from bson import ObjectId
 
 router = APIRouter()
